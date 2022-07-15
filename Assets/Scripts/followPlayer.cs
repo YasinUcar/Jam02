c6f7baf79cd24f13833a0196388f655a9c900223
Yasin Uçar <yasinucar@protonmail.com>
2022-07-16 01:52:47 +0300

    followChacter

---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class followPlayer : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent nav;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(target.position);
+
+
+        float fark = Vector3.Distance(target.transform.position, this.transform.position);
+
+        if (fark <= 2f)
+        {
+            GetComponent<Animator>().SetTrigger("Attack");
+        }
+
+
+    }
+}
